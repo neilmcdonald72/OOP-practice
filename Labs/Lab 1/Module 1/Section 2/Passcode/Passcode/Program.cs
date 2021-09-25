5f1dd68537129e890cd9d794b471c5d221db1059
Neil McDonald <w0225318@campus.nscc.ca>
2021-09-24 22:57:05 -0300

    section 2 commit

---
+++ Labs/Lab 1/Module 1/Section 2/Passcode/Passcode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the pass code?");
+            var code = Console.ReadLine();
+
+            if (code == "secret")
+            {
+                Console.WriteLine("Authenticated");
+                Console.WriteLine("Change the passcode");
+                code = Console.ReadLine();
+                Console.WriteLine("Your new password is {0}", code);
+
+
+            }
+            else if (code != "secret")
+            {
+                Console.WriteLine("Not Authenticated");
+            }
+        }
+    }
+}
+
+//Here is a simpler option without the use of if statements.
+
+
+//    class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+
+//        string code = null;
+//        while (code != "secret")
+//        {
+//            Console.WriteLine("Not Authenticated");
+//            Console.WriteLine("What is the pass code?");
+//            code = Console.ReadLine();
+//        }
+
+//        Console.WriteLine("Authenticated");
+//        Console.WriteLine("Change the passcode");
+//        code = Console.ReadLine();
+//        Console.WriteLine("Your new password is {0}", code);
+//    }
+//}
