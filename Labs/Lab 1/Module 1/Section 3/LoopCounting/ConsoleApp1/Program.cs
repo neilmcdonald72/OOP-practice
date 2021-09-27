c6b11cc54efedea2aa00544bf3ea14ace8eaf602
Neil McDonald <w0225318@campus.nscc.ca>
2021-09-26 21:36:32 -0300

    loop counting

---
+++ Labs/Lab 1/Module 1/Section 3/LoopCounting/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int z = 1; z <= 5; z++)
+            {
+
+                for (int i = 0, x = 0, y = 0; y <= 20; i += x)
+                {
+
+                    Console.WriteLine(i);
+
+                    if (i == 0) { x = 1; }
+
+                    if (i == 10) { x = -1; }
+
+                    y++;
+
+                }
+            }
+
+        }
+    }
+}
