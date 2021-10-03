1933f375bb4650577ff3d20918993309a7b3cc0f
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-03 18:14:20 -0300

    Module 3

---
+++ Labs/Lab 1/Module 3/Module3/Module3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                try
+                {
+                    var newStudent = new Student();
+
+                    newStudent.Name = MyCustomException.Ask("Student Name: ");
+
+                    newStudent.Grade = MyCustomException.AskInt("Student Grade: ");
+
+                    newStudent.Birthday = MyCustomException.Ask("Student Birthday: ");
+
+                    newStudent.Address = MyCustomException.Ask("Student Address: ");
+
+                    newStudent.Phone = MyCustomException.AskInt("Student Phone Number: ");
+
+                    students.Add(newStudent);
+                    Student.Count++;
+                    Console.WriteLine("Student Count: {0}", Student.Count);
+
+                    Console.WriteLine("Add another? y/n");
+
+                    if (Console.ReadLine() != "y")
+                        adding = false;
+                }
+                catch (FormatException msg)
+                {
+                    Console.WriteLine(msg.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error adding student, Please try again");
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
+            }
+        }
+
+        static void Import()
+        {
+            var importedStudent = new Student("Jenny", 86, "birthday", "address", 123456);
+            Console.WriteLine(importedStudent.Name);
+        }
+    }
+
+    class Member
+    {
+        public string Name;
+        public string Address;
+        protected int phone;
+
+        public int Phone
+        {
+            set { phone = value; }
+        }
+    }
+
+    class Student : Member
+    {
+        static public int Count = 0;
+
+        public int Grade;
+        public string Birthday;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int grade, string birthday, string address, int phone)
+        {
+            Name = name;
+            Grade = grade;
+            Birthday = birthday;
+            Address = address;
+            Phone = phone;
+        }
+    }
+
+    class Teacher : Member
+    {
+        public string Subject;
+    }
+
+    class MyCustomException : FormatException
+    {
+        static public string Ask(string question)
+        {
+            System.Console.Write(question);
+            return System.Console.ReadLine();
+        }
+
+        static public int AskInt(string question)
+        {
+            try
+            {
+                System.Console.Write(question);
+                return int.Parse(System.Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Input was not a number");
+            }
+        }
+    }
+}
