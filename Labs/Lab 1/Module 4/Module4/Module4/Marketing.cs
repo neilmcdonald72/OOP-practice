97d4af2c20049a835493037d25dd4de1b90ceb8c
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-03 22:04:20 -0300

    module4sect8

---
+++ Labs/Lab 1/Module 4/Module4/Module4/Marketing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    class Marketing
+    {
+        public void Start()
+        {
+            Program.Posted += HasPosted;
+        }
+        void HasPosted()
+        {
+            Console.WriteLine("Thank you for completing the survey. You are new subscribed to ten of our newsletters.");
+        }
+    }
+}
--- Labs/Lab 1/Module 4/Module4/Module4/Program.cs
+++ Labs/Lab 1/Module 4/Module4/Module4/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Module4
 {
+    class Data
+    {
+        public string Name;
+        public int Age;
+        public string Month;
+
+        public void Display()
+        {
+            Console.WriteLine("Your name is: {0}", Name);
+            Console.WriteLine("Your age is: {0}", Age);
+            Console.WriteLine("Your birth month is: {0}", Month);
+
+            if (Month == "march")
+            {
+                Console.WriteLine("you are an Aries.");
+            }
+            else if (Month == "april")
+            {
+                Console.WriteLine("you are a Taurus.");
+            }
+            else if (Month == "may")
+            {
+                Console.WriteLine("you are a Gemini.");
+            }
+        }
+    }
     class Program
     {
+        static public event Action Posted;
         static void Main(string[] args)
         {
+            Stats stats = new Stats();
+            stats.Start();
+            Marketing marketing = new Marketing();
+            marketing.Start();
+
+            var data = new Data();
+
+            Console.WriteLine("What is your name?");
+            data.Name = TryAnswer();
+
+            Console.WriteLine("What is your age?");
+            data.Age = int.Parse(TryAnswer());
+
+            Console.WriteLine("What month were you born in?");
+            data.Month = TryAnswer();
+
+            if (Posted != null)
+                Posted();
+
+            data.Display();
+        }
+
+        static string TryAnswer()
+        {
+            var question = Console.ReadLine();
+            if (question == "")
+            {
+                Console.WriteLine("You didn't type anything, please try again:");
+                return Console.ReadLine();
+            }
+            return question;
         }
     }
 }
