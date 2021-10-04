0288c608e2eb59bbd691b503e443dc82018ebcee
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-03 23:21:44 -0300

    sect 7 #1

---
+++ Labs/Lab 1/Module 4/Module4Sect7/SchoolTracker/Payroll.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTracker
+{
+    interface IPayee
+    {
+        void Pay();
+    }
+    class Payroll
+    {
+        Teacher teacher1 = new Teacher();
+        Teacher teacher2 = new Teacher();
+        Principal principal = new Principal();
+
+        public void PayAll()
+        {
+            teacher1.Pay();
+            teacher2.Pay();
+            principal.Pay();
+        }
+
+    }
+}
