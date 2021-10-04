1b6eca0371665da4682053bdd8f733cc6a1d84ee
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-03 22:54:05 -0300

    commit mod 4 sect 2=5

---
+++ Labs/Lab 1/Module 4/Module4Sect2-5/outreference/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace outreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+
+            Console.WriteLine(x);
+
+            Five(out x);
+
+            Console.WriteLine(x);
+
+            Double(ref x);
+
+            Console.WriteLine(x);
+
+            var test = "1.5";
+            var outDouble = 0.0;
+
+            Console.WriteLine("Result: {0}", ParseDouble(test, out outDouble));
+            Console.WriteLine("Test type is {0}", outDouble.GetType());
+            Console.WriteLine("Test value is {0}", outDouble);
+            test = "Ronan";
+            outDouble = 0.0;
+            Console.WriteLine("Result: {0}", ParseDouble(test, out outDouble));
+            Console.WriteLine("Test value is {0}", outDouble);
+
+
+        }
+
+        static void Five(out int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+
+        static void Double(ref int a)
+        {
+            a = a * 2;
+            Console.WriteLine(a);
+        }
+
+        static bool ParseDouble(string testString, out double outDouble)
+        {
+            outDouble = 0.0;
+            bool result = false;
+            try
+            {
+                if (double.Parse(testString).GetType() == outDouble.GetType())
+                {
+                    outDouble = double.Parse(testString);
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
