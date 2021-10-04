f47be360d2a726fd3137a8a92d75386db6f4fb39
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-03 23:33:27 -0300

    module4sect2

---
+++ Labs/Lab 1/Module 4/Module4Sect7Ver2/SchoolTracker/Payroll.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTracker
+{
+    interface IPayee
+    {
+        void Pay();
+    }
+    class Payroll
+    {
+        List<IPayee> payees = new List<IPayee>();
+        public Payroll()
+        {
+            payees.Add(new Teacher());
+            payees.Add(new Teacher());
+            payees.Add(new Principal());
+
+        }
+
+
+        public void PayAll()
+        {
+            foreach(var payee in payees)
+            {
+                payee.Pay();
+            }
+        }
+
+    }
+}
