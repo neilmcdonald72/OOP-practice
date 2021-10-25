0177a4169bf4b4b38ce56a778dd717b8363b9697
Neil McDonald <w0225318@campus.nscc.ca>
2021-10-25 11:38:38 -0300

    Part 1

---
+++ Assignments/Assignment1/SimpleChat/SimpleChat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Part 1
+            //Run as client vs server
+            Console.WriteLine("Listening for messages");
+            //User input mode. one hitting "I" key
+            var userKey = Console.ReadKey();
+            //Display incoming message
+
+            //Allow for input
+
+            //Let user quit
+            Console.WriteLine($"You typed {userKey.Key}");
+            Console.Beep();
+
+        }
+    }
+}
